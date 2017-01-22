0d007ca7b2e2832fb8a065cfd54808de5dafb1f8
Assen Kolov <assen.kolov@gmail.com>
2017-01-22 13:56:08 +0100

    login works locally with config server

--- src/main/docker/zookeeper-initializer/init-zookeeper-env.sh
+++
@@ -1,7 +0,0 @@
-
-$CMD -server $ZK create /config/application/redis.host redis
-$CMD -server $ZK create /config/application/mongodb.host mongodb
-
-$CMD -server $ZK create /config/application/spring.data.mongodb.uri=mongodb://mongo:27017/curri
-$CMD -server $ZK create /config/application/spring.data.mongodb.database=curri
-$CMD -server $ZK create /config/application/spring.data.mongodb.name=curriculi
