af943cbfafbcc0aca8bfb5b9fe757a0ea42dae77
Assen Kolov <assen.kolov@gmail.com>
2017-01-24 10:54:40 +0100

    config server

---
+++ config-server/src/main/scala/curri/config/ConfigServerApp.scala
@@ -0,0 +1,18 @@
+package curri.config
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@SpringBootApplication
+@EnableConfigServer
+class ConfigServerApp {
+
+}
+
+object ConfigServerApp {
+
+  def main(args: Array[String]) {
+    SpringApplication.run(classOf[ConfigServerApp])
+  }
+}
