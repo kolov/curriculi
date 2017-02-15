bf81ad76484f9b606d29cef52acea37ec8120101
Assen Kolov <assen.kolov@gmail.com>
2017-02-15 17:30:16 +0100

    deploying to kubernetes from local docker repository

--- README.md
+++ README.md
@@ -37,3 +37,16 @@ secrets are kept.
     ./gradlew build buildImages
     docker-compose -f src/main/docker/docker-compose.yml up
     Go to http://curri.xip.io:8080/index
+    
+## Kubernetes
+
+### Local registry
+   docker run -v /data/docker/registry:/var/lib/registry -d -p 5000:5000 --restart=always --name registry registry:2
+
+	 CURRI_ROOT=~/projects/curriculi
+	 kubectl delete configmap curri-config
+   kubectl create configmap curri-config --from-file=$CURRI_ROOT/secrets/config-server/config-server.properties
+   kubectl delete -f $CURRI_ROOT/src/main/k8s/config-server-d.yml
+   kubectl apply -f $CURRI_ROOT/src/main/k8s/config-server-d.yml
+   kubectl delete -f $CURRI_ROOT/src/main/k8s/config-server-s.yml
+   kubectl apply -f $CURRI_ROOT/src/main/k8s/config-server-s.yml
---
+++ src/main/k8s/kinstall.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+CURRI_ROOT=~/projects/curriculi
+kubectl delete configmap curri-config
+sh  $CURRI_ROOT/secrets/config-server/set-config-server-config.sh
+
+kubectl delete -f $CURRI_ROOT/src/main/k8s/config-server-d.yml
+kubectl apply -f $CURRI_ROOT/src/main/k8s/config-server-d.yml
+kubectl delete -f $CURRI_ROOT/src/main/k8s/config-server-s.yml
+kubectl apply -f $CURRI_ROOT/src/main/k8s/config-server-s.yml
+
+
+kubectl delete -f $CURRI_ROOT/src/main/k8s/eureka-d.yml
+kubectl apply -f $CURRI_ROOT/src/main/k8s/eureka-d.yml
+kubectl delete -f $CURRI_ROOT/src/main/k8s/eureka-s.yml
+kubectl apply -f $CURRI_ROOT/src/main/k8s/eureka-s.yml
+
+kubectl delete -f $CURRI_ROOT/src/main/k8s/service-users-d.yml
+kubectl apply -f $CURRI_ROOT/src/main/k8s/service-users-d.yml
+kubectl delete -f $CURRI_ROOT/src/main/k8s/service-users-s.yml
+kubectl apply -f $CURRI_ROOT/src/main/k8s/service-users-s.yml
