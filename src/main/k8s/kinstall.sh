bd2b2a397a1b741ef91f663acbe9e62be017aa0d
Assen Kolov <assen.kolov@gmail.com>
2017-02-20 22:14:35 +0100

    tag/push

--- src/main/k8s/kinstall.sh
+++ src/main/k8s/kinstall.sh
@@ -1,26 +1,36 @@
 #!/usr/bin/env bash
 
-CURRI_ROOT=~/projects/curriculi
+# Comment out otr replace the following line
+export CURRI_ROOT=~/projects/curriculi
+
+if [ -z ${CURRI_ROOT+x} ]; then
+  echo "CURRI_ROOT is unset"
+  exit 1;
+else
+  echo "CURRI_ROOT is set to '$CURRI_ROOT'";
+fi
+
 kubectl delete configmap curri-config
 sh  $CURRI_ROOT/secrets/config-server/set-config-server-config.sh
 
 kubectl delete -f $CURRI_ROOT/src/main/k8s/config-server-d.yml
 kubectl apply -f $CURRI_ROOT/src/main/k8s/config-server-d.yml
-kubectl delete -f $CURRI_ROOT/src/main/k8s/config-server-s.yml
 kubectl apply -f $CURRI_ROOT/src/main/k8s/config-server-s.yml
 
 
 kubectl delete -f $CURRI_ROOT/src/main/k8s/eureka-d.yml
 kubectl apply -f $CURRI_ROOT/src/main/k8s/eureka-d.yml
-kubectl delete -f $CURRI_ROOT/src/main/k8s/eureka-s.yml
 kubectl apply -f $CURRI_ROOT/src/main/k8s/eureka-s.yml
+kubectl apply -f $CURRI_ROOT/src/main/k8s/eureka-lb.yml
 
 kubectl delete -f $CURRI_ROOT/src/main/k8s/mongo-users-d.yml
 kubectl apply -f $CURRI_ROOT/src/main/k8s/mongo-users-d.yml
-kubectl delete -f $CURRI_ROOT/src/main/k8s/mongo-users-s.yml
 kubectl apply -f $CURRI_ROOT/src/main/k8s/mongo-users-s.yml
 
 kubectl delete -f $CURRI_ROOT/src/main/k8s/service-users-d.yml
 kubectl apply -f $CURRI_ROOT/src/main/k8s/service-users-d.yml
-kubectl delete -f $CURRI_ROOT/src/main/k8s/service-users-s.yml
 kubectl apply -f $CURRI_ROOT/src/main/k8s/service-users-s.yml
+
+kubectl delete -f $CURRI_ROOT/src/main/k8s/redis-d.yml
+kubectl apply -f $CURRI_ROOT/src/main/k8s/redis-d.yml
+kubectl apply -f $CURRI_ROOT/src/main/k8s/redis-s.yml
