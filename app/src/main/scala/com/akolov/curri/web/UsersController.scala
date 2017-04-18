62bb3dcfa2a1cec2f9bd00a9604def305585090d
Assen Kolov <assen.kolov@gmail.com>
2017-04-18 21:39:20 +0200

    updates user in session after change

--- app/src/main/scala/com/akolov/curri/web/UsersController.scala
+++ app/src/main/scala/com/akolov/curri/web/UsersController.scala
@@ -1,6 +1,6 @@
 package com.akolov.curri.web
 
-import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.{HttpServletRequest, HttpSession}
 
 import com.akolov.curri.service.user.client.UsersClient
 import com.akolov.curri.service.user.domain.User
@@ -21,10 +21,14 @@ class UsersController(private val usersClient: UsersClient) {
 
   @RequestMapping(value = Array("/accepts-cookies"), method = Array(RequestMethod.POST))
   @ResponseBody
-  def acceptsCookies(servletRequest: HttpServletRequest): User = {
+  def acceptsCookies(req: HttpServletRequest, session: HttpSession): User = {
 
-    val user = servletRequest.getAttribute(CookieFilter.ATTR_USER_NAME).asInstanceOf[User]
-     usersClient.acceptsCookies(user.id)
+    val user = req.getAttribute(CookieFilter.ATTR_USER_NAME).asInstanceOf[User]
+    val updated = usersClient.acceptsCookies(user.id)
+    if (updated != null) {
+      session.setAttribute(CookieFilter.ATTR_USER_NAME, updated)
+    }
+    updated
   }
 
   @RequestMapping(value = Array(""), method = Array(RequestMethod.GET))
