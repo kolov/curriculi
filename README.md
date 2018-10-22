b09755d9dd5236624e32b3dff7129c1da16efb4b
Assen Kolov <assen.kolov@gmail.com>
2018-10-22 21:30:38 +0200

    support for DO

--- README.md
+++ README.md
@@ -4,12 +4,34 @@ Scripts to install curri
 
 Before running the scripts, the following environment variables must be defined:
 GTILAB_REG_TOKEN
-URRI_GOGGLE_CLIENT_ID
-CURRI_GOOGLE_CLIENT_SECRET
 
-To install on GKE:
-./src/main/k8s/curri-gke-all.sh
+To create cluster on GKE and deploy the application:
 
-To install on any Kubernetes, after kubectl has been configured:
-./src/main/k8s/curri-k8s.sh
+    ./src/main/k8s/curri-gke-all.sh
+
+To deploy on any Kubernetes, after `kubectl` has been configured:
+
+    ./src/main/k8s/curri-k8s.sh provider
+
+where provider=do,mininkube
+
+## Tricks
+
+To get service account token from a cluster, prepare file secret.json:
+
+    {
+        "kind": "Secret",
+        "apiVersion": "v1",
+        "metadata": {
+            "name": "mysecretname",
+            "annotations": {
+                "kubernetes.io/service-account.name": "default"
+            }
+        },
+        "type": "kubernetes.io/service-account-token"
+    }
+
+    kubectl create -f secret.json
+    kubectl get secrets -n kube-system
+
 
--- k8s/curri-k8s.sh
+++ k8s/curri-k8s.sh
@@ -15,12 +15,21 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
+if [ -z "$GTILAB_REG_TOKEN" ]; then
+  echo GTILAB_REG_TOKEN is not defined
+  usage
+  exit 1
+fi
+
+
+
 CURRI_DEST=$1
 echo Will install to destination $CURRI_DEST
 
 echo Will install curri in kubernetes context: $(kubectl config current-context)
 
 Echo Setting secrets...
+kubectl delete secret regcred 2> /dev/null
 
 kubectl create secret docker-registry regcred --docker-server=registry.gitlab.com \
     --docker-username=assen.kolov@gmail.com \
@@ -30,12 +39,13 @@ $DIR/../secrets/curri-k8s-config.sh
 
 if [[ -z "$CURRI_PROJECT" ]]; then
    CURRI_PROJECT="$( cd "$( dirname "$DIR/../../.." )" && pwd )"
-   echo CURRI_PROJECT is not set
+   echo CURRI_PROJECT was not set, set to $CURRI_PROJECT
 fi
 echo Will use CURRI_PROJECT=$CURRI_PROJECT
 $CURRI_PROJECT/curri-users/k8s/deploy-all-$CURRI_DEST.sh
 $CURRI_PROJECT/curri-gate/k8s/deploy-all-$CURRI_DEST.sh
 $CURRI_PROJECT/curri-elm/k8s/deploy-all-$CURRI_DEST.sh
+$CURRI_PROJECT/curriculi-nginx/k8s/deploy-all-$CURRI_DEST.sh
 
-kubectl apply -f $DIR/ingress-$CURRI_DEST.yml
+# kubectl apply -f $DIR/ingress-$CURRI_DEST.yml
 
